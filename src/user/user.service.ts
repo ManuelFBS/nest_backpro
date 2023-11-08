a73894dbecbcb1eba04dd9e2a0919a206464a5e8
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-08 10:14:29 -0400

    Actualizacion de user.service.ts

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,15 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  /**
+   * Aquí, hemos utilizado el enfoque del mapeador de datos para este
+   * tutorial, es por eso que inyectamos el repositorio aquí. Otro enfoque
+   * puede ser registros activos.
+   */
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    const user: User = new User();
+
+    user.name = createUserDto.name;
+    user.age = createUserDto.age;
+    user.email = createUserDto.email;
+    user.username = createUserDto.username;
+    user.password = createUserDto.password;
+    user.gender = createUserDto.gender;
+
+    return await this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async findAllUsers(): Promise<User[]> {
+    return await this.userRepository.find();
   }
 
   findOne(id: number) {
