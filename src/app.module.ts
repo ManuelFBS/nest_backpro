bf27d75fce3d8e2e5a38ad250a80b20d7b2c56ac
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-08 02:51:33 -0400

    Modificacion de app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
 import { UserModule } from './user/user.module';
 
 @Module({
@@ -18,7 +17,6 @@ import { UserModule } from './user/user.module';
       synchronize: true,
       logging: true,
     }),
-    UsersModule,
     UserModule,
   ],
   controllers: [AppController],
