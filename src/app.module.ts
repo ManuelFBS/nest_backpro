4199877aed8393da0b9062a2c6928081d8df721a
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-08 13:13:25 -0400

    Modificacion de app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,26 +4,28 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
-import { User } from './user/entities/user.entity';
+// import { User } from './user/entities/user.entity';
+import { DataSourceConfig } from './config/data.source';
 
-const configService = new ConfigService();
+// const configService = new ConfigService();
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: configService.get('DB_HOST'),
-      port: configService.get('DB_PORT'),
-      password: configService.get('DB_PASSWORD'),
-      username: configService.get('DB_USERNAME'),
-      entities: [User],
-      database: configService.get('DB_NAME'),
-      synchronize: true,
-      logging: false,
-    }),
+    // TypeOrmModule.forRoot({
+    //   type: 'postgres',
+    //   host: configService.get('DB_HOST'),
+    //   port: configService.get('DB_PORT'),
+    //   password: configService.get('DB_PASSWORD'),
+    //   username: configService.get('DB_USERNAME'),
+    //   entities: [User],
+    //   database: configService.get('DB_NAME'),
+    //   synchronize: true,
+    //   logging: false,
+    // }),
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
     UserModule,
   ],
   controllers: [AppController],
