577e6695e083f86226d8b3ee189609b1a3c230cf
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-08 23:12:27 -0400

    Creacion de auth.module.ts, Actual. app.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,9 +4,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { DataSourceConfig } from './config/data.source';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [TypeOrmModule.forRoot({ ...DataSourceConfig }), UserModule],
+  imports: [
+    TypeOrmModule.forRoot({ ...DataSourceConfig }),
+    UserModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
