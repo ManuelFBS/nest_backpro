c6f71f2689a91f6da7646ac4b481b14b736d3ad9
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-09 12:53:44 -0400

    Actualizacion de auth.service.ts

--- src/auth/services/auth.service.ts
+++ src/auth/services/auth.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 import { UserService } from '../../user/user.service';
 
 @Injectable()
 export class AuthService {
   constructor(private readonly userService: UserService) {}
 
-  public async validateUser(username: string, password: string) {}
+  public async validateUser(username: string, password: string) {
+    const userByUsername = await this.userService.findBy({
+      key: 'username',
+      value: username,
+    });
+    const userByEmail = await this.userService.findBy({
+      key: 'email',
+      value: username,
+    });
+
+    if (userByUsername) {
+      const match = await bcrypt.compare(password, userByUsername.password);
+      if (match) return userByUsername;
+    }
+
+    if (userByEmail) {
+      const match = await bcrypt.compare(password, userByEmail.password);
+      if (match) return userByEmail;
+    }
+
+    return null;
+  }
 }
