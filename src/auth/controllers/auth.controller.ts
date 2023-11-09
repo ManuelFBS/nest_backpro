cbfbcb05122196d5c171f4dc9b59f768f523aa48
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-09 14:06:53 -0400

    Actualizacion y modificacion de auth.controller.ts

--- src/auth/controllers/auth.controller.ts
+++ src/auth/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Body, Controller, Post, UnauthorizedException } from '@nestjs/common';
-import { AuthBody } from '../interfaces/auth.interface';
 import { AuthService } from '../services/auth.service';
+import { AuthDTO } from '../dto/auth.dto';
 
 /* La clase AuthController maneja las solicitudes de autenticación, incluido el inicio de sesión, y
 genera un token web JSON (JWT) para el usuario autenticado. */
@@ -8,10 +8,16 @@ genera un token web JSON (JWT) para el usuario autenticado. */
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
-  /* El decorador `@Post('login')` se utiliza para especificar que este método debe manejar solicitudes
-  POST al punto final '/login'. */
   @Post('login')
-  public async login(@Body() { username, password }: AuthBody) {
+  /**
+   * La función de inicio de sesión ingresa un nombre de usuario y contraseña, valida al usuario,
+   * genera un token JWT y lo devuelve.
+   * @param {AuthDTO}  - - `@Body()`: este es un decorador utilizado en algunos marcos (por ejemplo,
+   * NestJS) para indicar que el siguiente parámetro debe extraerse del cuerpo de la solicitud.
+   * @returns un JSON Web Token (JWT) después de validar las credenciales del usuario y generar el
+   * token.
+   */
+  public async login(@Body() { username, password }: AuthDTO) {
     const userValidate = await this.authService.validateUser(
       username,
       password,
