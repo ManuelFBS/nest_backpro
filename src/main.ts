4c7a1a7d6a9e48ba31323123d5601316b1be290c
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-10 07:18:28 -0400

    Actualizacion de los packages (.json) y modificacion de main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
+import { ClassSerializerInterceptor } from '@nestjs/common';
 import { AppModule } from './app.module';
+import * as morgan from 'morgan';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(morgan('dev'));
+
+  const reflector = app.get(Reflector);
+
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(reflector));
+
   await app.listen(3000);
 }
 bootstrap();
