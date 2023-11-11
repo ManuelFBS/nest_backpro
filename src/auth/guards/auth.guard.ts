4cfebac0fcb0e4019783c032994a0b63fc738e3b
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-11 15:23:41 -0400

    Actualizacion de auth.guard.ts

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -1,7 +1,14 @@
-import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import {
+  CanActivate,
+  ExecutionContext,
+  Injectable,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
-import { Observable } from 'rxjs';
+import { PUBLIC_KEY } from '../../constants/key-decorators';
 import { UserService } from '../../user/services/user.service';
+import { IUseToken } from '../interfaces/auth.interface';
+import { useToken } from 'src/utils/use.token';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -11,6 +18,32 @@ export class AuthGuard implements CanActivate {
   ) {}
 
   async canActivate(context: ExecutionContext) {
+    const isPublic = this.reflector.get<boolean>(
+      PUBLIC_KEY,
+      context.getHandler(),
+    );
+
+    if (isPublic) {
+      return true;
+    }
+
+    const req = context.switchToHttp().getRequest<Request>();
+    const token = req.headers['nest_back'];
+
+    if (!token || Array.isArray(token)) {
+      throw new UnauthorizedException('Invalid token...!!!');
+    }
+
+    const manageToken: IUseToken | string = useToken(token);
+
+    if (typeof manageToken === 'string') {
+      throw new UnauthorizedException(manageToken);
+    }
+
+    if (manageToken.isExpired) {
+      throw new UnauthorizedException('TokenExpired...');
+    }
+
     return true;
   }
 }
