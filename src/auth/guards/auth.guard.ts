c49f02580a237845352f3d9eeef6f1999c8b8342
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-11 23:32:26 -0400

    Actualizacion d auth.guard.ts

--- src/auth/guards/auth.guard.ts
+++ src/auth/guards/auth.guard.ts
@@ -5,10 +5,12 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
+import { Request } from 'express';
 import { PUBLIC_KEY } from '../../constants/key-decorators';
 import { UserService } from '../../user/services/user.service';
 import { IUseToken } from '../interfaces/auth.interface';
 import { useToken } from 'src/utils/use.token';
+import { User } from 'src/user/entities/user.entity';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
@@ -43,6 +45,15 @@ export class AuthGuard implements CanActivate {
     if (manageToken.isExpired) {
       throw new UnauthorizedException('TokenExpired...');
     }
+
+    const { sub } = manageToken;
+    const user = await this.userService.findUser(parseInt(sub));
+
+    if (!user) {
+      throw new UnauthorizedException('Invalid user...!!!');
+    }
+
+    req.idUser = user.id.toString();
 
     return true;
   }
